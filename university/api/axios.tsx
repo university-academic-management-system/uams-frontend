ab03351c671a6914f5ed98f2ceacaef86e8afcb9
HaroldOE <harold.ekempka@speedlinkng.com>
2026-01-15 11:20:32 +0100

    upaddtaed base url

--- university/api/axios.tsx
+++ university/api/axios.tsx
@@ -24,7 +24,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
