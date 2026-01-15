1658524bb77135fb29d4657ac539fe936157c452
Lloyd-Emperor <honourakhigbe6@gmail.com>
2026-01-15 16:02:18 +0100

    major changes1

--- students/src/pages/auth/components/AuthBackground.tsx
+++ students/src/pages/auth/components/AuthBackground.tsx
@@ -4,7 +4,7 @@ const AuthBackground: React.FC = () => (
   <div className="fixed inset-0 w-full h-full z-0 font-['Inter']">
     <img
       src={`${import.meta.env.BASE_URL}assets/login-image.png`}
-      alt="EduFlow Login Background"
+      alt="UAMS Login Background"
       className="absolute inset-0 w-full h-full object-cover"
     />
     <div className="absolute inset-0 bg-black/40"></div>
