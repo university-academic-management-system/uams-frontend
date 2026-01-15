43594d0f8249e8e0cee3edf1fdbb8909bbe64908
Edward Ndiyo <edward.ndiyo@speedlinkng.com>
2026-01-15 20:45:57 +0100

    updated123

--- departmental/api/axios.tsx
+++ departmental/api/axios.tsx
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: "http://localhost:5000/",
-
-  
-
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: "http://localhost:5000/",
   headers: {
     "Content-Type": "application/json",
   },
